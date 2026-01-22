135a1f53c9ccbf1b8345ecf8f771b12e23b405a9
GalTor85 <gal85@bk.ru>
2026-01-22 16:15:46 +0300

    Created project

---
+++ src/main/java/ru/galtor85/housetrade/HousetradeApplication.java
@@ -0,0 +1,13 @@
+package ru.galtor85.housetrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousetradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousetradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/galtor85/housetrade/HousetradeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.galtor85.housetrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousetradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
